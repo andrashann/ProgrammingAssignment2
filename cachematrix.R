e54351b5b96f42fa8088f87694c258e45c99bc42
András Hann <andrashann@users.noreply.github.com>
2014-09-21 23:59:56 +0200

    cache inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We are going to get the inverse of a matrix. Once it is calcualted, it is stored in
+## a cache until another matrix is loaded. In this case, the inverse is recalculated.
 
-## Write a short comment describing this function
+## This function creates a list of functions that get and set the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL                              ## Set inverse to NULL when function is called
+    set <- function(y) {                     ## If we SET the matrix, it is assigned to the
+        x <<- y                              ##   stored object and the inverse is deleted
+        inv <<- NULL
+    }
+    get <- function() x                      ## This returns the stored matrix
+    setinv <- function(inv) inv <<- inv      ## Sets the inverse
+    getinv <- function() inv                 ## Gets the inverse
+    list(set = set, get = get,               ## Creates a list of functions and returns it
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function tries to get the inverse from chache, if it does not find it, it
+## is calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    m <- x$getinv()                         ## Get the inverse from cache
+    if(!is.null(m)) {                       ## If not null, return stored value (and exit fn)
+        message("getting cached data")
+        return(m)
+    }                                       ## If was null,
+    data <- x$get()                         ## Read the matrix into the data
+    m <- solve(data, ...)                   ## Calculate inverse (see ?solve)
+    x$setinv(m)                             ## Store this inverse
+    m}
